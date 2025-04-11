5657b460103948c64dca744ea2d07caf5d8dccca
Miguel Velásquez <86170247+MrM1nd91@users.noreply.github.com>
2025-04-10 22:27:59 -0300

    Código inicial para mostrar el total y el valor

--- TP2-Grupo12/Ejercicio1.aspx.cs
+++ TP2-Grupo12/Ejercicio1.aspx.cs
@@ -16,9 +16,16 @@ namespace TP2_Grupo12
 
         protected void btnGenerarTabla_Click(object sender, EventArgs e)
         {
+            
             string tablaObjetos = "<table border='1'>";
             tablaObjetos += "<tr><th>Producto</th><th>Cantidad</th></tr>";
-            tablaObjetos += "<tr><td" + txtProducto.Text + "</td><td>" + txtCantidad.Text + "</td></tr>";
+            
+            tablaObjetos += "<tr><td>" + txtProducto.Text + "</td><td>" + txtCantidad.Text + "</td></tr>";
+            
+            tablaObjetos += "<tr><td>" + txtProducto2.Text + "</td><td>" + txtCantidad2.Text + "</td></tr>";
+
+            tablaObjetos += "<tr><td>total</td><td>" + (int.Parse(txtCantidad.Text))/*Hay agregar la ultima parte*/ ;
+
         }
     }
 }
